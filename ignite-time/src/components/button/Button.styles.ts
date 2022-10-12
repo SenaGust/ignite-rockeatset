7b2e3ae9da23d2e672afc60c46da08939b588c13
Gustavo Sena <gustavofs@ciandt.com>
2022-10-12 19:31:39 -0300

    All files

---
+++ ignite-time/src/components/button/Button.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import { ButtonVariant } from './Button.types'
+
+interface ButtonContainerProps {
+  variant: ButtonVariant
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+  background-color: ${({ variant, theme }) => theme['green-300']};
+`
