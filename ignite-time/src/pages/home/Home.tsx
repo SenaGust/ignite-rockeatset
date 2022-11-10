34ae8c55cad9f8bb1a3a9f6452608534d4fae4c6
Gustavo Sena <gustavofs@ciandt.com>
2022-11-10 18:41:55 -0300

    feat(ignite-timer): Implemented countdown

--- ignite-time/src/pages/home/Home.tsx
+++ ignite-time/src/pages/home/Home.tsx
@@ -11,6 +11,8 @@ import {
 } from "./Home.styles";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from "zod";
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from "date-fns";
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "task name should have more than 1 character"),
@@ -22,7 +24,29 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
+interface Cycle {
+  id: string;
+  task: string;
+  countdownInMinutes: number;
+  startDate: Date;
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<Array<Cycle>>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+  const countDownInSeconds = (activeCycle?.countdownInMinutes || 0) * 60;
+  const currentSeconds = activeCycle
+    ? countDownInSeconds - amountSecondsPassed
+    : 0;
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+
+  const minutesFormatted = String(minutesAmount).padStart(2, "0");
+  const secondsFormatted = String(secondsAmount).padStart(2, "0");
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -30,13 +54,46 @@ export function Home() {
       task: "",
     },
   });
+
   const hasTask = Boolean(watch("task"));
   const isSubmitDisabled = !hasTask;
 
-  function handleCreateNewCycle(data: any) {
-    console.log(data);
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    const id = String(new Date().getTime());
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      countdownInMinutes: data.countdownInMinutes,
+      startDate: new Date(),
+    };
+
+    setAmountSecondsPassed(0);
+    setCycles((state) => [...state, newCycle]);
+    setActiveCycleId(id);
     reset();
   }
+
+  useEffect(() => {
+    let interval: number;
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate)
+        );
+      }, 1);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [activeCycle]);
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutesFormatted}:${secondsFormatted}`;
+    }
+  }, [minutesFormatted, secondsFormatted]);
 
   return (
     <HomeContainer>
@@ -73,11 +130,11 @@ export function Home() {
         </FormContainer>
 
         <CountDownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutesFormatted[0]}</span>
+          <span>{minutesFormatted[1]}</span>
           <CountDownSeparator>:</CountDownSeparator>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondsFormatted[0]}</span>
+          <span>{secondsFormatted[1]}</span>
         </CountDownContainer>
 
         <StartCountdownButton disabled={isSubmitDisabled} type="submit">
