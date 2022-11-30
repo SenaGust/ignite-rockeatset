fe200690b40c28a41815f9fc398641c67e9c1d16
Gustavo Sena <gustavofs@ciandt.com>
2022-11-29 23:33:13 -0300

    feat(dt-money): implemented transaction table

--- dt-money/src/pages/transactions/TransactionsPage.tsx
+++ dt-money/src/pages/transactions/TransactionsPage.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/header/Header";
 import { Summary } from "../components/summary/Summary";
+import {
+  TransactionsContainer,
+  TransactionsTable,
+  PriceHighlight,
+} from "./TransactionsPage.styles";
 
 export function TransactionsPage() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- $ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
