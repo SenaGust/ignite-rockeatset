3957f9c63811e0338dafe018fc7e9b1f9e47746b
Gustavo Sena <gustavofs@ciandt.com>
2023-01-09 17:19:43 -0300

    feat(design-system): added tsc to build the lib

--- design-system/packages/tokens/src/font-sizes.ts
+++ design-system/packages/tokens/src/font-sizes.ts
@@ -2,7 +2,7 @@ export const fontSizes = <const>{
   xxs: "0.625rem",
   xs: "0.75rem",
   sm: "0.875rem",
-  md: "1rem",
+  md: "1rem", // equals to 16px
   lg: "1.125rem",
   xl: "1.25rem",
   "2xl": "1.5rem",
--- design-system/packages/tokens/src/space.ts
+++ design-system/packages/tokens/src/space.ts
@@ -2,7 +2,7 @@ export const space = <const>{
   1: "0.25rem",
   2: "0.5rem",
   3: "0.75rem",
-  4: "1rem",
+  4: "1rem", // equals to 16px
   5: "1.25rem",
   6: "1.5rem",
   7: "1.75rem",
