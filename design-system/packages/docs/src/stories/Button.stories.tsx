4c802bcc5020e31cb887275470151051bd22f347
Gustavo Sena <gustavofs@ciandt.com>
2023-01-11 17:49:09 -0300

    feat(design-system): added button stories

---
+++ design-system/packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button, ButtonProps } from '@ignite-ui/react';
+
+export default {
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'Send',
+  },
+} as Meta<ButtonProps>;
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'small',
+  },
+};
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+  },
+};
