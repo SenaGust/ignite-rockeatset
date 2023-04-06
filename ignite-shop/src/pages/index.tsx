050b5c4e12a3e73d8515dc0dac4ffdb0a853dd1c
Gustavo Sena <gustavofs@ciandt.com>
2023-04-05 21:29:59 -0300

    Configurando documento HTML

--- ignite-shop/src/pages/index.tsx
+++ ignite-shop/src/pages/index.tsx
@@ -1,3 +1,14 @@
+import { styled } from "../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+});
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <Button>button</Button>
+    </div>
+  );
 }
